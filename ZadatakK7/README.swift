cac84c51a9db44bc64e832c7289d74b9a365551e
Q <nemanjak@hushmail.com>
2021-11-07 18:41:49 +0100

    Added Readme

---
+++ ZadatakK7/README.swift
@@ -0,0 +1,14 @@
+
+
+#warning("Readme")
+
+// Napomene
+//
+// - UI na slikama nije konzistentan. Obično zatvaranja, brisanje i dugmići tog tipa idu levo. Ja sam radio kao na slikama.
+// - Gledao sam da uradim sta se konkretno trazi, uz poneku sitnu apstrakciju.
+// - Što se GIT-a tiče, dodavao sam po testovima, ali može i sitnije da se razbije, ako vam je to fazon.
+//
+// To je otprilike to. Ako mi je nešto promaklo, javite.
+//
+// Pozdrav
+
